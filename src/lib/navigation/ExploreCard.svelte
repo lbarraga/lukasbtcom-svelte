d9f320abd7b47d5d0339403d2f7bfed21b9822bc
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-07-31 00:23:50 +0200

    add explore card

---
+++ src/lib/navigation/ExploreCard.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let title;
+    export let description;
+    export let icon;
+    export let link = '/';
+</script>
+
+<a href="{link}" class="card card-hover p-4 flex items-center space-x-4">
+    <!-- Icon -->
+    <span class="flex items-center justify-center">
+        <i class="fa-solid {icon} text-3xl"></i>
+    </span>
+
+    <!-- Content -->
+    <div class="flex-1">
+        <h3 class="text-xl font-semibold mb-1">{title}</h3>
+        <p>{description}</p>
+    </div>
+</a>
