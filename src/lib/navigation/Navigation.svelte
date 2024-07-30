34d8cf0c36896bf79fe7605e454d0e8a39ce6269
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-07-31 00:22:42 +0200

    add navigation component to use in left-drawer

---
+++ src/lib/navigation/Navigation.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { getDrawerStore } from "@skeletonlabs/skeleton";
+
+    const drawerStore = getDrawerStore();
+
+    function drawerClose() {
+        drawerStore.close();
+    }
+
+</script>
+
+<nav class="list-nav p-4">
+    <ul>
+        <li><a href="/" on:click={drawerClose}>Home</a></li>
+        <li><a href="/skills" on:click={drawerClose}>Skills</a></li>
+        <li><a href="/resume" on:click={drawerClose}>Resume</a></li>
+        <li><a href="/portfolio" on:click={drawerClose}>Portfolio</a></li>
+        <li><a href="/contact" on:click={drawerClose}>Contact</a></li>
+    </ul>
+</nav>
