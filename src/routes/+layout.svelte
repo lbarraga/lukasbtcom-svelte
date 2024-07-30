45f787572aa30c4d9a7a20d92eab2ed686700f97
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-07-31 00:24:36 +0200

    add hamburger + app drawer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,56 @@
 <script>
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/navigation/Navigation.svelte';
+	import { initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+	initializeStores();
+
+	const drawerStore = getDrawerStore();
+
+	function drawerOpen() {
+		drawerStore.open({});
+	}
 </script>
 
+<Drawer>
+	<Navigation />
+</Drawer>
+
 <!-- App Shell -->
-<AppShell>
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 md:w-64">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center">
+					<button class="md:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<a href="/">
+						<strong class="text-base uppercase">Lukas Barragan Torres</strong>
+					</a>
+				</div>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord!
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<div class="hidden md:flex space-x-4">
+					<a class="btn hover:variant-soft-primary" href="/skills">Skills</a>
+					<a class="btn hover:variant-soft-primary" href="/resume">Resume</a>
+					<a class="btn hover:variant-soft-primary" href="/portfolio">Portfolio</a>
+					<a class="btn hover:variant-soft-primary" href="/contact">Contact</a>
+				</div>
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
+
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
