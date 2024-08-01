5aa295eaceba7de4673e2c0fdd02ded6091e78d0
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-08-02 00:19:43 +0200

    first contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,28 @@
-<h1>Contact me</h1>
+<div class="m-10">
+    <h1 class="text-center text-3xl font-extrabold">Contact</h1>
+    <p class="w-full text-center font-bold mt-4">
+        If you have any questions or would like to collaborate, don't hesitate to reach out.
+    </p>
+</div>
+
+<div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-5 m-10">
+
+    <!-- GitHub -->
+    <a class="logo-item" target="_blank" href="https://github.com/lbarraga">
+        <span><i class="fa-brands fa-github fa-lg"></i></span>
+        <span>GitHub</span>
+    </a>
+
+    <!-- Linkedin -->
+    <a class="logo-item" target="_blank" href="https://www.linkedin.com/in/lukas-barragan-torres/">
+        <span><i class="fa-brands fa-linkedin fa-lg"></i></span>
+        <span>Linkedin</span>
+    </a>
+
+    <!-- Email -->
+    <a class="logo-item" target="_blank" href="mailto:elbarragan03@gmail.com">
+        <span><i class="fa-solid fa-envelope fa-lg"></i></span>
+        <span>Email</span>
+    </a>
+</div>
+
