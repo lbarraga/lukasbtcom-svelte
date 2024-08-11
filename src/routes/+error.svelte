785b295485e4689ea709d9cd2648454424a6d66d
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-08-12 00:53:22 +0200

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<div class="text-center space-y-8">
+    <div class="text-9xl font-extrabold opacity-30 mt-20">404</div>
+    <div class="text-4xl font-bold m-5">You found a secret place</div>
+    <p class="m-5">
+        Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been
+        moved to another URL.
+    </p>
+    <div>
+        <a href="/" class="hover:underline">Take me back to home page</a>
+    </div>
+</div>
