b3899cf1b60124a99d3c0f4aa516be7889ea3666
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2024-08-22 18:59:12 +0200

    use contact json instead of duplicating

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import contact from '$lib/data/contact.json';
+</script>
+
 <div class="m-10">
     <h1 class="text-center text-3xl font-extrabold">Contact</h1>
     <p class="w-full text-center font-bold mt-4">
@@ -5,24 +9,12 @@
     </p>
 </div>
 
-<div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-5 m-10">
-
-    <!-- GitHub -->
-    <a class="logo-item" target="_blank" href="https://github.com/lbarraga">
-        <i class="iconify text-xl" data-icon="fa6-brands:github"></i>
-        <span>GitHub</span>
-    </a>
-
-    <!-- Linkedin -->
-    <a class="logo-item" target="_blank" href="https://www.linkedin.com/in/lukas-barragan-torres/">
-        <i class="iconify text-xl" data-icon="fa6-brands:linkedin"></i>
-        <span>Linkedin</span>
-    </a>
-
-    <!-- Email -->
-    <a class="logo-item" target="_blank" href="mailto:elbarragan03@gmail.com">
-        <i class="iconify text-xl" data-icon="fa6-solid:envelope"></i>
-        <span>Email</span>
-    </a>
+<div class="logo-cloud grid-cols-1 lg:!grid-cols-2 gap-5 m-10">
+    {#each contact as { href, icon, text }}
+        <a class="logo-item" href={href} target="_blank">
+            <i class="iconify" data-icon={icon}></i>
+            <span>{text}</span>
+        </a>
+    {/each}
 </div>
 
